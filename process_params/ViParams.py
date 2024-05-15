ef969eaac7dc8c887c9a03e4a73fdb591a21bde6
Vodopivec, Andres <andres.vodopivec@evotec.com>
2024-05-15 08:54:06 -0700

    added VI params

--- process_params/ViParams.py
+++ process_params/ViParams.py
@@ -11,9 +11,10 @@ class ViParams(Params):
     def __init__(
         self,
         stopHighVolumePercent: float,
-        highVolumePercent: float,
-        lowVolumePercent: float,
-        stopLowVolumePercent: float,
+        upsLowFlowCompPercent: float,
+        highLevelCompPercent: float,
+        LowLevelPercent: float,
+        lowLowLevelCompPercent: float,
         minWorkinfVolume: float,
         acidAdditionPercent: float,
         baseAdditionPercent: float,
@@ -24,9 +25,10 @@ class ViParams(Params):
     ) -> None:
 
         self.stopHighVolumePercent = stopHighVolumePercent  # %
-        self.highVolumePercent = highVolumePercent  # %
-        self.lowVolumePercent = lowVolumePercent  # %
-        self.stopLowVolumePercent = stopLowVolumePercent  # %
+        self.upsLowFlowCompPercent = upsLowFlowCompPercent  # %
+        self.highLevelCompPercent = highLevelCompPercent  # %
+        self.LowLevelPercent = LowLevelPercent  # %
+        self.lowLowLevelCompPercent = lowLowLevelCompPercent  # %
         self.minWorkinfVolume = minWorkinfVolume  # L
         self.acidAdditionPercent = acidAdditionPercent  # %
         self.baseAdditionPercent = baseAdditionPercent  # %
@@ -34,6 +36,17 @@ class ViParams(Params):
         self.baseBuffer = baseBuffer
         self.efficiency = efficiency
         self.reactionTime = reactionTime  # h
+
+        # Calculated variables
+        self.stopHighMass = self.minWorkinfVolume * \
+            (1 + self.stopHighVolumePercent / 100)
+        self.highMassUspSwitchLow = self.minWorkinfVolume * \
+            (1 + self.upsLowFlowCompPercent / 100)
+        self.highMass = self.minWorkinfVolume * \
+            (1 + self.highLevelCompPercent / 100)
+        self.LowMass = self.minWorkinfVolume * (1 - self.LowLevelPercent / 100)
+        self.lowLowMass = self.minWorkinfVolume * \
+            (1 - self.lowLowLevelCompPercent / 100)
 
         return None
 
@@ -54,14 +67,20 @@ class ViParams(Params):
     def __str__(self) -> str:
         return f'''
         {self.__class__.__name__}:
-            stopHighVolumePercent: {self.stopHighVolumePercent}
-            highVolumePercent: {self.highVolumePercent}
-            lowVolumePercent: {self.lowVolumePercent}
-            stopLowVolumePercent: {self.stopLowVolumePercent}
-            minWorkinfVolume: {self.minWorkinfVolume}
-            acidAdditionPercent: {self.acidAdditionPercent}
-            baseAdditionPercent: {self.baseAdditionPercent}
+            stopHighVolumePercent: {self.stopHighVolumePercent}%
+            upsLowFlowCompPercent: {self.upsLowFlowCompPercent}%
+            highLevelCompPercent: {self.highLevelCompPercent}%
+            LowLevelPercent: {self.LowLevelPercent}%
+            lowLowLevelCompPercent: {self.lowLowLevelCompPercent}%
+            minWorkinfVolume: {self.minWorkinfVolume} L
+            acidAdditionPercent: {self.acidAdditionPercent}%
+            baseAdditionPercent: {self.baseAdditionPercent}%
             acidBuffer: {self.acidBuffer}
             baseBuffer: {self.baseBuffer}
             efficiency: {self.efficiency}
-            reactionTime: {self.reactionTime}'''
+            reactionTime: {self.reactionTime}
+            stopHighMass: {self.stopHighMass}
+            highMassUspSwitchLow: {self.highMassUspSwitchLow}
+            highMass: {self.highMass}
+            LowMass: {self.LowMass}
+            lowLowMass: {self.lowLowMass}'''
